b01183601d1173293cbd3068ea180dd631d28472
Che.El <che.eldar@gmail.com>
2020-04-09 19:20:00 +0300

    product, customer, and order models

--- crm/accounts/admin.py
+++ crm/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Customer, Product, Order
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['date_created']
