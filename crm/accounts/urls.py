66d2b1322a02f0937a5cc9ce5fb0ac7634be63bc
Che.El <che.eldar@gmail.com>
2020-04-22 20:44:33 +0300

    creating multiple orders on the customer page

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home_url'),
     path('products/', views.products, name='product_url'),
     path('customer/<int:pk>/', views.customer, name='customer_url'),
-    path('create_order/', views.create_order, name='create_order_url'),
+    path('create_order/<int:pk>/', views.create_order, name='create_order_url'),
     path('update_order/<int:pk>/', views.update_order, name='update_order_url'),
     path('delete_order/<int:pk>/', views.delete_order, name='delete_order_url'),
 ]
